69c75db9b153b1153023915ed46b575a93ea90ec
Ivor Wanders <ivor@iwanders.net>
2024-09-24 19:03:58 -0400

    Opening the device.

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum H100iError {
+    #[error("no matching usb device found")]
+    NoDevice,
+    #[error("hid error occured")]
+    HidError(#[from] hidapi::HidError),
+}
+
+struct H100i {
+    api: hidapi::HidApi,
+    device: hidapi::HidDevice,
+}
+
+impl H100i {
+    pub fn new_h100i() -> Result<H100i, H100iError> {
+        // Bus 001 Device 003: ID 1b1c:0c35 Corsair
+        let vendor_id = 0x1b1c;
+        let product_id = 0x0c35;
+
+        let api = hidapi::HidApi::new()?;
+        let mut found_device = None;
+        for device in api.device_list() {
+            if device.vendor_id() == vendor_id && device.product_id() == product_id {
+                found_device = Some(device.open_device(&api)?);
+            }
+        }
+        if let Some(device) = found_device {
+            Ok(H100i { api, device })
+        } else {
+            Err(H100iError::NoDevice)
+        }
+    }
+}
