7882c8ffa0a1aedf48fbd228c1d8b46629fd37a0
Ivor Wanders <ivor@iwanders.net>
2024-11-02 19:35:50 -0400

    Use clap for the commandline.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Runs the development main function.
+    Develop,
+    // Add { name: Option<String> },
+}
+
 fn main() -> Result<(), h100i_tool::H100iError> {
-    h100i_tool::main()
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Develop => h100i_tool::main(),
+    }
 }
